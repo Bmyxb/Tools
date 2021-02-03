2fbe49aa582fd5f9c178610ee8fd151de53762ed
yxb <yangxingbo@imoran.net>
2021-02-03 16:58:34 +0800

    input args from cmd line

--- redis/clear_data_with_no_ttl.py
+++ redis/clear_data_with_no_ttl.py
@@ -1,13 +1,14 @@
 #!/bin/python
 # -*- coding:utf-8 -*-
 
+import sys
 import redis
 import logging
 from datetime import datetime
 
 # define logging style for removed keys
 def initLogSetting():
-    date = datetime.now().strftime('%y%m%d')
+    date = datetime.now().strftime('_%y%m%d_%H_%M')
     filename = './clear_' + date + '.log'
     logging.basicConfig(
         level = logging.INFO,
@@ -17,8 +18,8 @@ def initLogSetting():
         filemode = 'w'
     )
 
-# clear keys with no ttl
-def clear_no_ttl(host, port, password):
+# clear keys with no expire time
+def clear_no_expire(host, port, password):
     initLogSetting()
     r = redis.StrictRedis(host=host, port=port, password=password, socket_connect_timeout=10)
     n = 0
@@ -57,12 +58,26 @@ def clear_data_with_prefix(host, port, password, prefix):
     print("Total : %s"  % total_cnt)
     print("Removed : %s" % remove_cnt)
 
+def print_instruct():
+    print('Usage: python ', sys.argv[0], ' host port password op [prefix]')
+    print('op: ')
+    print(' clear_no_expire - clear all data with no expire time')
+    print(' clear_prefix - clear data starts with [prefix]')
 
 if __name__ == '__main__':
-    # 获取redis数据
-    host = '127.0.0.1'
-    port = 6379
-    password = ''
-    clear_no_ttl(host, port, password)
-    clear_data_with_prefix(host, port, password, "push")
-    
+    if len(sys.argv) < 5:
+        print_instruct()
+        exit(1)
+    db_host = sys.argv[1]
+    db_port = sys.argv[2]
+    db_password = sys.argv[3]
+    op = sys.argv[4]
+    if op == 'clear_no_expire':
+        clear_no_expire(db_host, db_port, db_password)
+    elif op == 'clear_prefix':
+        if len(sys.argv) != 6:
+            print_instruct()
+            exit(1)
+        prefix = sys.argv[6]
+        clear_data_with_prefix(db_host, db_port, db_password, prefix)
+
